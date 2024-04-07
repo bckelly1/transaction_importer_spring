3054ce8cfad35cc1dc8acc2db06c314e5571949e
Brian Kelly <wolfyman06@gmail.com>
2024-04-07 09:51:52 -0600

    fix imports

--- src/main/java/com/brian/transaction_importer_spring/controller/TransactionController.java
+++ src/main/java/com/brian/transaction_importer_spring/controller/TransactionController.java
@@ -1,8 +1,7 @@
 package com.brian.transaction_importer_spring.controller;
 
-import com.brian.transaction_importer_spring.entity.Transaction;
-import com.brian.transaction_importer_spring.repository.*;
-import com.brian.transaction_importer_spring.service.TransactionService;
+import com.brian.transaction_importer_spring.repository.CategoryRepository;
+import com.brian.transaction_importer_spring.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,24 +12,16 @@ import java.util.List;
 @Controller
 public class TransactionController {
     @Autowired
-    private AccountRepository accountRepository;
-
-    @Autowired
     private CategoryRepository categoryRepository;
 
     @Autowired
     private TransactionRepository transactionRepository;
 
-    @Autowired
-    private VendorRepository vendorRepository;
-
 
     @GetMapping("/transactions")
     public String showEntities(Model model) {
-//        model.addAttribute("accounts", accountRepository.findAll());
         model.addAttribute("categories", categoryRepository.findAll());
         model.addAttribute("transactions", transactionRepository.findTop10ByOrderByIdDesc());
-//        model.addAttribute("vendors", vendorRepository.findAllOrderByIdDesc());
         return "transactions";
     }
 }
--- src/main/java/com/brian/transaction_importer_spring/entity/Account.java
+++ src/main/java/com/brian/transaction_importer_spring/entity/Account.java
@@ -1,6 +1,12 @@
 package com.brian.transaction_importer_spring.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
