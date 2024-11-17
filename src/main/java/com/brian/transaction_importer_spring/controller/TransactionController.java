38b39aefcc465ca8ec34ea0895258159e871484b
Brian Kelly <wolfyman06@gmail.com>
2024-11-17 10:57:01 -0700

    add redirects after delete, fix favicon

--- src/main/java/com/brian/transaction_importer_spring/controller/TransactionController.java
+++ src/main/java/com/brian/transaction_importer_spring/controller/TransactionController.java
@@ -5,10 +5,15 @@ import com.brian.transaction_importer_spring.entity.Transaction;
 import com.brian.transaction_importer_spring.repository.CategoryRepository;
 import com.brian.transaction_importer_spring.repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequiredArgsConstructor
@@ -43,10 +48,13 @@ public class TransactionController {
         return "transactions";
     }
 
-    @GetMapping("/transaction/{transactionId}/delete")
-    public String deleteTransaction(@PathVariable(value="transactionId") Long transactionId, Model model) {
+    @PostMapping("/transaction/{transactionId}/delete")
+    @ResponseBody
+    public ResponseEntity<Void> deleteTransaction(@PathVariable(value="transactionId") Long transactionId) {
         System.out.println("Received delete transaction request for " + transactionId);
-//        transactionRepository.deleteById(transactionId);
-        return showEntities(model);
+        transactionRepository.deleteById(transactionId);
+
+        return ResponseEntity.ok().build();
+
     }
 }
